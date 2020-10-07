89df28f87ab8beed62d10f42521288e4c7f9d938
opob7414 <olivier.briand@orange.Com>
2020-10-07 12:53:18 +0200

    fix bug in listdata code

--- listdata.py
+++ listdata.py
@@ -3,7 +3,7 @@ from requestdata import sendRequest
 
 #### Main
 
-status, result = sendRequest("GET", https://obriand.fr/api/v1/list.php")
+status, result = sendRequest("GET", "https://obriand.fr/api/v1/list.php")
 
 if(status) :
 	print("\n==> list data result = " + str(result))

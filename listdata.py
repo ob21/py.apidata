b5a5d4d460bf22cac303daa725732ef84d190aab
opob7414 <olivier.briand@orange.Com>
2020-10-08 17:19:49 +0200

    add type criteria in listdata

--- listdata.py
+++ listdata.py
@@ -1,9 +1,24 @@
-
+import sys
 from requestdata import sendRequest
 
 #### Main
 
-status, result = sendRequest("GET", "https://obriand.fr/api/v1/list.php")
+# Get and check arg
+arg = ""
+try:
+  arg = sys.argv[1]
+  print("type=" + arg)
+except:
+  print("info: no type found in arguments")
+
+# Prepare url
+url = "https://obriand.fr/api/v1/list.php"
+if arg:
+  url += "?type="+arg
+
+# Process request
+
+status, result = sendRequest("GET", url)
 
 if(status) :
 	print("\n==> list data result = " + str(result))

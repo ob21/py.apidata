36822727af693354bc14a48730251a202a7ce8cf
opob7414 <olivier.briand@orange.Com>
2020-10-09 12:52:50 +0200

    define a function for listdata

--- listdata.py
+++ listdata.py
@@ -1,26 +1,30 @@
 import sys
 from requestdata import sendRequest
 
-#### Main
+#### List data
+
+# Define list function
+def list():
 
 # Get and check arg
-arg = ""
-try:
-  arg = sys.argv[1]
-  print("type=" + arg)
-except:
-  print("info: no type found in arguments")
+  arg = ""
+  try:
+    arg = sys.argv[1]
+    #print("type=" + arg)
+  except:
+    print("info: no type found in arguments")
 
 # Prepare url
-url = "https://obriand.fr/api/v1/list.php"
-if arg:
-  url += "?type="+arg
+  url = "https://obriand.fr/api/v1/list.php"
+  if arg:
+    url += "?type="+arg
 
 # Process request
+  status, result = sendRequest("GET", url)
+  if(status) :
+    return result
+  else:
+    return "has failed"
 
-status, result = sendRequest("GET", url)
-
-if(status) :
-	print("\n==> list data result = " + str(result))
-else:
-	print("\n==> list data has failed")
+# Main
+print("\n==> list data result = " + str(list()))
